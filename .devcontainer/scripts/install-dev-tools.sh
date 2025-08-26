c3d8dfa0a9f823583d9bbb9ca1bc4f88458923ea
Pavel Bezglasnyy <pavelb23@gmail.com>
2025-08-26 12:01:07 +0200

    Update cuda dev container config

--- .devcontainer/scripts/install-dev-tools.sh
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Run this command from the PyTorch directory after cloning the source code using the “Get the PyTorch Source“ section below
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 git submodule sync
 git submodule update --init --recursive
 
